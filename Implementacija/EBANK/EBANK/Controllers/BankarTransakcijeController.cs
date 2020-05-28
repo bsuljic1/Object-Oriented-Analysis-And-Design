d5eb33498ada3b62d52aa7f357f5c2de69b93a55
asiljak1 <asiljak1@etf.unsa.ba>
2020-05-28 13:24:31 +0200

    prepravke u view-ovima

---
+++ Implementacija/EBANK/EBANK/Controllers/BankarTransakcijeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EBANK.Data;
+using EBANK.Models;
+
+namespace EBANK.Controllers
+{
+    public class BankarTransakcijeController : Controller
+    {
+        private readonly OOADContext _context;
+
+        public BankarTransakcijeController(OOADContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BankarTransakcije
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transakcija.ToListAsync());
+        }
+
+        // GET: BankarTransakcije/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transakcija = await _context.Transakcija
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (transakcija == null)
+            {
+                return NotFound();
+            }
+
+            return View(transakcija);
+        }
+
+        // GET: BankarTransakcije/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BankarTransakcije/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Vrijeme,Iznos,VrstaTransakcije,NacinTransakcije")] Transakcija transakcija)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transakcija);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transakcija);
+        }
+
+        // GET: BankarTransakcije/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transakcija = await _context.Transakcija.FindAsync(id);
+            if (transakcija == null)
+            {
+                return NotFound();
+            }
+            return View(transakcija);
+        }
+
+        // POST: BankarTransakcije/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Vrijeme,Iznos,VrstaTransakcije,NacinTransakcije")] Transakcija transakcija)
+        {
+            if (id != transakcija.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transakcija);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransakcijaExists(transakcija.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transakcija);
+        }
+
+        // GET: BankarTransakcije/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transakcija = await _context.Transakcija
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (transakcija == null)
+            {
+                return NotFound();
+            }
+
+            return View(transakcija);
+        }
+
+        // POST: BankarTransakcije/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transakcija = await _context.Transakcija.FindAsync(id);
+            _context.Transakcija.Remove(transakcija);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransakcijaExists(int id)
+        {
+            return _context.Transakcija.Any(e => e.Id == id);
+        }
+    }
+}
