43f584e8b18c0ba4aaeb028d48b0dec48d017189
Berina Suljic <bsuljic1@etf.unsa.ba>
2020-05-30 20:40:52 +0200

    Dodana konverzija

---
+++ Implementacija/EBANK/EBANK/Models/Konverzija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EBANK.Models
+{
+    public class Konverzija
+    {
+        public int Id { get; set; }
+        [DisplayName("Iznos")]
+        public float Iznos { get; set; }
+        [DisplayName("Iz valute")]
+        public Valuta IzValute { get; set; }
+        [DisplayName("U valutu")]
+        public Valuta UValutu { get; set; }
+        
+    }
+}
