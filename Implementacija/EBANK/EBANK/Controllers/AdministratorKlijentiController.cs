cb760419b321e9b5d0a12baed3f7cf69399a1d92
Berina Suljic <bsuljic1@etf.unsa.ba>
2020-05-30 17:52:05 +0200

    AdministratorKlijenti uradjeno

--- Implementacija/EBANK/EBANK/Controllers/AdministratorKlijentiController.cs
+++ Implementacija/EBANK/EBANK/Controllers/AdministratorKlijentiController.cs
@@ -7,22 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EBANK.Data;
 using EBANK.Models;
+using EBANK.Models.KlijentRepository;
 
 namespace EBANK.Controllers
 {
     public class AdministratorKlijentiController : Controller
     {
-        private readonly OOADContext _context;
-
+        IKlijenti _klijenti;
         public AdministratorKlijentiController(OOADContext context)
         {
-            _context = context;
+            _klijenti = new KlijentiProxy(context);
         }
 
         // GET: AdministratorKlijenti
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Klijent.ToListAsync());
+            return View(await _klijenti.DajSveKlijente());
         }
 
         // GET: AdministratorKlijenti/Details/5
@@ -33,8 +33,7 @@ namespace EBANK.Controllers
                 return NotFound();
             }
 
-            var klijent = await _context.Klijent
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var klijent = await _klijenti.DajKlijenta(id);
             if (klijent == null)
             {
                 return NotFound();
@@ -44,110 +43,10 @@ namespace EBANK.Controllers
         }
 
         // GET: AdministratorKlijenti/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: AdministratorKlijenti/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DatumRodjenja,Spol,JMBG,BrojTelefona,BrojLicneKarte,Zanimanje,Ime,Prezime,KorisnickoIme,Lozinka")] Klijent klijent)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(klijent);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(klijent);
-        }
-
-        // GET: AdministratorKlijenti/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var klijent = await _context.Klijent.FindAsync(id);
-            if (klijent == null)
-            {
-                return NotFound();
-            }
-            return View(klijent);
-        }
-
-        // POST: AdministratorKlijenti/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("DatumRodjenja,Spol,JMBG,BrojTelefona,BrojLicneKarte,Zanimanje,Ime,Prezime,KorisnickoIme,Lozinka")] Klijent klijent)
-        {
-            if (id != klijent.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(klijent);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!KlijentExists(klijent.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(klijent);
-        }
-
-        // GET: AdministratorKlijenti/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var klijent = await _context.Klijent
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (klijent == null)
-            {
-                return NotFound();
-            }
-
-            return View(klijent);
-        }
-
-        // POST: AdministratorKlijenti/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var klijent = await _context.Klijent.FindAsync(id);
-            _context.Klijent.Remove(klijent);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+        
         private bool KlijentExists(int id)
         {
-            return _context.Klijent.Any(e => e.Id == id);
+            return _klijenti.DaLiPostojiKlijent(id);
         }
     }
 }
