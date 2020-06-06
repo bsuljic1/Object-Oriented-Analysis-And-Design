b2cdd4f4e3c45b99e7011afe2d90dc2f48250791
esuljic <enver13579@windowslive.com>
2020-06-06 21:48:37 +0200

    Konverzija

---
+++ Implementacija/EBANK/EBANK/Controllers/KlijentKonverzijaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EBANK.Data;
+using EBANK.Models;
+
+namespace EBANK.Controllers
+{
+    public class KlijentKonverzijaController : Controller
+    {
+        private readonly OOADContext _context;
+
+        public KlijentKonverzijaController(OOADContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Konverzija
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
--- Implementacija/EBANK/EBANK/Data/OOADContext.cs
+++ Implementacija/EBANK/EBANK/Data/OOADContext.cs
@@ -36,7 +36,5 @@ namespace EBANK.Data
             modelBuilder.Entity<Transakcija>().ToTable("Transakcija");
             modelBuilder.Entity<ZahtjevZaKredit>().ToTable("ZahtjevZaKredit");
         }
-        //Ova funkcija se koriste da bi se ukinulo automatsko dodavanje množine u nazive
-        public DbSet<EBANK.Models.Konverzija> Konverzija { get; set; }
     }
 }
